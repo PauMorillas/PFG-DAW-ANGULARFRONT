5a5d7259c81cb460bdb8dc124ea4258fca1a2907
PauMorillas <morillashuertapau@gmail.com>
2025-11-27 16:53:01 +0100

    Base de la implementacion hecha

---
+++ src/app/components/negocios-generar-script/negocios-generar-script.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { environment } from '../../../environments/environment';
+
+@Component({
+  selector: 'app-negocios-generar-script',
+  imports: [],
+  templateUrl: './negocios-generar-script.html',
+  styleUrl: './negocios-generar-script.css',
+})
+export class NegociosGenerarScript implements OnInit{
+  idNegocio!: number;
+  script!: string;
+  
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const apiBaseUrl = environment.apiBaseUrl ?? '';
+    this.idNegocio = Number(this.route.snapshot.paramMap.get('id'));
+    this.script = `<script src="${apiBaseUrl}js/widget-embed.js" data-service-id="${this.idNegocio}" type="application/javascript"></script>`;
+  }
+}
